2fc86b48720fccfb453047f0abce67a43a19b9e0
Abhijeet Srivastav <56226285+AbhijeetSrivastav@users.noreply.github.com>
2020-10-02 11:32:28 +0530

    Update twitter.py
    Well commented the twitter.py

--- twitter.py
+++ twitter.py
@@ -1,23 +1,30 @@
+#Import the required modules
 import tweepy
 import time
 import os
 
+#Setting the API Keys and tokens
 consumer_api_key = os.environ.get('consumer_api_key')
 consumer_api_secret = os.environ.get('consumer_api_secret')
 access_token = os.environ.get('access_token')
 access_token_secret = os.environ.get('access_token_secret')
 
+#Creating instance of tweepy authentication handler
 auth = tweepy.OAuthHandler(consumer_api_key, consumer_api_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 
+#Creating instance of tweepy API handler
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
+#Setting the user API
 user = api.me()
 
+#List of the terms to query and number of tweets to watch for
 searchArr = ['#javascript', '#typescript', '#python']
 nTweets = 5000
 
+#Searching for the querries on twitter
 for search in searchArr:
     for tweet in tweepy.Cursor(api.search, search).items(nTweets):
         try:
